bad1e9ecd7d9d63ffb5f20506e5787ca73c38f1a
Selim <selimdev00@gmail.com>
2023-12-16 14:57:29 +0500

    [API]: fetching random quotes

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// write your JavaScript here
+'use strict'
+
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
+
+const quoteText = document.getElementById('quote-text')
+const quoteAuthor = document.getElementById('quote-author')
+const newQuoteButton = document.getElementById('new-quote-button')
+
+async function fetchRandomQuote() {
+    const response = await fetch(RANDOM_QUOTE_API_URL)
+    const data = await response.json()
+    return { author: data.author, content: data.content }
+}
+
+async function appendQuoteToTemplate() {
+    const {author, content } = await fetchRandomQuote()
+    quoteText.innerHTML = content
+    quoteAuthor.innerHTML = author
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    await appendQuoteToTemplate()
+
+    newQuoteButton.addEventListener('click', async () => {
+        await appendQuoteToTemplate()
+    })
+})
