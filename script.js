965fd3ae0cc66b00086766954ff6644f6c38a725
Selim <selimdev00@gmail.com>
2023-12-16 15:09:57 +0500

    [loading]: added loading status with helper functions

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
 const quoteText = document.getElementById('quote-text')
 const quoteAuthor = document.getElementById('quote-author')
 const newQuoteButton = document.getElementById('new-quote-button')
+const box = document.getElementById('quote-box')
+
+let loading = false
 
 async function fetchRandomQuote() {
     const response = await fetch(RANDOM_QUOTE_API_URL)
@@ -12,10 +15,34 @@ async function fetchRandomQuote() {
     return { author: data.author, content: data.content }
 }
 
+function setLoading(status) {
+    if (status) {
+        box.classList.add('is-loading')
+        loading = true
+    } else {
+        box.classList.remove('is-loading')
+        loading = false
+    }
+}
+
+async function loadingWithCallback(fn) {
+    if (loading) return
+
+    newQuoteButton.disabled = true
+
+    setLoading(true)
+    await fn()
+    setLoading(false)
+
+    newQuoteButton.disabled = false
+}
+
 async function appendQuoteToTemplate() {
-    const {author, content } = await fetchRandomQuote()
-    quoteText.innerHTML = content
-    quoteAuthor.innerHTML = author
+    await loadingWithCallback(async () => {
+        const {author, content } = await fetchRandomQuote()
+        quoteText.innerHTML = content
+        quoteAuthor.innerHTML = author
+    })
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
